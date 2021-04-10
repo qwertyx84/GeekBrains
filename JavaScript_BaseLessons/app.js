edccd5a99c7fc69b15f6ddc9cd67098f191ccc19
Oleg.Nemtyrev <qwertyx84@mail.ru>
2021-04-10 14:53:47 +0400

    First commit

---
+++ JavaScript_BaseLessons/app.js
@@ -0,0 +1,15 @@
+ "use strict";
+ alert('hello \' world');
+ console.log('Вывод текста в консоль, например для просмотра "промежуточных результатов"')
+ console.info('Hello' + ' world!')
+ console.warn(2 + 3)
+ console.error('Hello world!')
+ console.table(['Hello', 'world!'])
+ 
+
+ let greetings="Всем привет" //предпочтительней let / const. Связано с областью видимости
+ const Max = 10
+ var num = 45
+ alert(greetings)
+ /*sdfsdfs*/ 
+ //fgdfd
